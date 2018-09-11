a90efe564e0f33f410f98d54e6f76d3e430aac09
Yvonne Reyes <panganibanyvonne.yp@gmail.com>
2018-09-11 19:05:17 -0400

    last commit

---
+++ resources/views/include/navbar.blade.php
@@ -0,0 +1,12 @@
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white">
+	<h5 class="my-0 mr-md-auto font-weight-normal">{{config('app.name', 'SERYE')}}</h5>
+    <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="/">HOME</a>
+        <a class="p-2 text-dark" href="/shop">SHOP</a>
+        <a class="p-2 text-dark" href="#">NEW ARRIVALS</a>
+        <a class="p-2 text-dark" href="#">CATEGORIES</a>
+        <a class="p-2 text-dark" href="#">CREATE AN ACCOUNT</a>
+		<a class="p-2 text-dark" href="#">CONTACT</a>
+    </nav>
+    <a class="btn btn-outline-primary" href="#">LOG IN</a>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,18 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>SERYE Online Shop</title>
+
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+    {{-- Custom CSS --}}
+	<link rel="stylesheet" href="{{asset('sass/app.scss')}}">
+
+    <title>{{config('app.name', 'SERYE')}}</title>
+</head>
 
     <body>
-    	@yield('content')
+    	@include('include.navbar')
+    		@yield('content')
     </body>
 
 </html>
