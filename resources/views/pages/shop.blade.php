10c4ff78ef8d9d1e8c5fe465b5e2a6f7ce5dc7db
Yvonne Reyes <panganibanyvonne.yp@gmail.com>
2018-09-11 17:53:25 -0400

    first commit

---
+++ resources/views/pages/shop.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Shop</h1>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () { // URL custom name
+//     return view('welcome'); // Blade name
+// });
+
+
+// Route::get('/shop', function () { // URL custom name
+//     return view('pages.shop'); // Blade location
+// });
+
+Route::get('/', 'PagesController@index');
+
+Route::get('shop', 'PagesController@shop');
+
