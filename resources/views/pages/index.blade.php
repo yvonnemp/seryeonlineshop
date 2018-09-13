e963e84bffd8c519bd5df53b08b5802b8f91c8a4
Yvonne Reyes <panganibanyvonne.yp@gmail.com>
2018-09-13 19:25:44 -0400

    Initial Commit, fixed the image path

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,43 +1,21 @@
 @extends('layouts.app') 
 @section('content')
-<header>
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner" role="listbox">
-            <!-- Slide One - Set the background image for this slide in the line below -->
-            <div class="carousel-item active">
-                <div class="carousel-caption d-none d-md-block">
-                    <h3>First Slide</h3>
-                    <p>This is a description for the first slide.</p>
-                </div>
-            </div>
-            <!-- Slide Two - Set the background image for this slide in the line below -->
-            <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
-                <div class="carousel-caption d-none d-md-block">
-                    <h3>Second Slide</h3>
-                    <p>This is a description for the second slide.</p>
-                </div>
-            </div>
-            <!-- Slide Three - Set the background image for this slide in the line below -->
-            <div class="carousel-item" style="background-image: url('http://placehold.it/1900x1080')">
-                <div class="carousel-caption d-none d-md-block">
-                    <h3>Third Slide</h3>
-                    <p>This is a description for the third slide.</p>
-                </div>
-            </div>
+<header class="showcase">
+    <div class="content">
+        <img src="logo.png" alt="SERYE" class="logo">
+        <div class="title">
+            Welcome to SERYE Online Shop
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
+        <div class="text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, ipsam.
+        </div>
     </div>
 </header>
+<section class="services">
+    <div class="container grid-3">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+</section>    
 @endsection
